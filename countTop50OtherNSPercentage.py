b1f8d1191b1fdcda5ab2732bf930dd0595460694
kwang40 <kaishenw1995@gmail.com>
2019-03-13 21:17:20 -0500

    fix bug

--- countTop50OtherNSPercentage.py
+++ countTop50OtherNSPercentage.py
@@ -1,15 +1,19 @@
-unqiueDomains = {}
+uniqueDomains = {}
 with open ('tokenNS.txt') as f:
     for line in f:
-        unqiueDomains[line[:line.find(':')]] = True
+        domains = line[line.find(':')+1 : ].split(',')
+        for domain in domains:
+            uniqueDomains[domain] = True
 print ('Token NS unqiue Domains count: ' + str(len(uniqueDomains)))
 
-unqiueDomains = {}
+uniqueDomains = {}
 with open ('otherNS.txt') as f:
     countLine = 0
     for line in f:
          countLine += 1
          if countLine == 51:
-             print ('Top 50 other NS unique Domain Count: ' + str(len(unqiueDomains)))
-        unqiueDomains[line[:line.find(':')]] = True
+             print ('Top 50 other NS unique Domain Count: ' + str(len(uniqueDomains)))
+         domains = line[line.find(':')+1 : ].split(',')
+         for domain in domains:
+             uniqueDomains[domain] = True
 print ('Other NS unqiue Domains count: ' + str(len(uniqueDomains)))
