bed958be88752b9cd67e75b175dbf7ad524eeb77
kwang40 <kaishenw1995@gmail.com>
2019-03-18 14:17:28 -0500

    update

--- random100.py
+++ random100.py
@@ -1,15 +1,17 @@
 parkedDomains = {}
 with open('fullParkedDomains.txt') as f:
     for line in f:
-        parkedDomains[line.rstrip()] == True
+        parkedDomains[line.rstrip()] = True
 
 count = 0
 lineCount = 0
 with open('validDomains.txt') as f:
-    if count == 100:
-        break
     for line in f:
+        lineCount += 1
+        if count == 100:
+            break
         if lineCount % 800 != 0:
             continue
         if line.rstrip() not in parkedDomains:
-            print(line.rtrip())
+            count += 1
+            print(line.rstrip())
