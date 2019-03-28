17b242cb1d32c3ecb6dbacf0869aeb55b181d526
kwang40 <kaishenw1995@gmail.com>
2019-03-28 12:41:19 -0500

    update

--- investNonIDParkedDomains.py
+++ investNonIDParkedDomains.py
@@ -1,5 +1,6 @@
+import json
 idDomains = {}
-
+nonIdDomains = {}
 for file in ['domainsByNewID.txt','domainsByOldID.txt']:
     with open (file) as f:
         for line in f:
@@ -9,6 +10,12 @@ with open ('fullParkedDomains.txt') as f:
     with open ('nonIDParkedDomains.txt', 'w') as f2:
         for line in f:
             if line.rstrip() not in idDomains:
+                nonIdDomains[line.rstrip()] = True
                 f2.write(line)
 
-        
+with open ('/data1/nsrg/kwang40/fullData/2019-03-03/banners.json') as f:
+    with open('nonIDParkedData.json') as f2:
+        for line in f:
+            data = json.loads(line.rstrip())
+            if data['domain'] in nonIdDomains:
+                f2.write(line)
